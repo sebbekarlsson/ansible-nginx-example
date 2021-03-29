14c79947bdfb43ce667aad66075f5354cffc5d35
ianertson <sebbekarlsson97@gmail.com>
2021-03-29 11:34:47 +0200

    adds more tests

--- tests/test_site.py
+++ tests/test_site.py
@@ -1,7 +1,22 @@
 import os
 
-def test_site_has_correct_text():
+
+def test_site_has_html_tag():
+    fp = open(os.path.join(os.path.dirname(__file__), '../playbooks/site/index.html'))
+    contents = fp.read()
+
+    assert '<html lang="en">' in contents
+
+
+def test_site_mentions_my_name():
     fp = open(os.path.join(os.path.dirname(__file__), '../playbooks/site/index.html'))
     contents = fp.read()
 
     assert 'Sebastian' in contents
+
+
+def test_site_has_welcome_text():
+    fp = open(os.path.join(os.path.dirname(__file__), '../playbooks/site/index.html'))
+    contents = fp.read()
+
+    assert 'Welcome' in contents
